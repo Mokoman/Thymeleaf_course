60c9bd5d1762d455a4cca6939ce5ee3b927a806f
Marek Motyl <motyl.marek@gmail.com>
2024-05-26 09:49:08 +0200

    update to use static images

--- learn/src/main/java/com/example/learn/controllers/ProductController.java
+++ learn/src/main/java/com/example/learn/controllers/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
   
-  @RequestMapping("/product")
+  @RequestMapping({"/product", "/product.html"})
   public String getProducts() {
     return "product";
   }
